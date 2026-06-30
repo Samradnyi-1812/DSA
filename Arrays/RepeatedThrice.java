849e1e880b0277d19846dca99b45a04ca28ef159
Samradnyi-1812 <samradnyisawant18@gmail.com>
2026-06-30 21:54:22 +0530

    Optimal Solution to find the number repeated thrice

---
+++ Arrays/RepeatedThrice.java
@@ -0,0 +1,37 @@
+// To find the number repeated thrice
+//* Optimal Approach: Using HashMap */
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class RepeatedThrice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter the array elements:");
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Count frequency
+        for(int i = 0; i < n; i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        // Print the number repeated thrice
+        for(int key : map.keySet()){
+            if(map.get(key) == 3){
+                System.out.println("Number repeated thrice: " + key);
+            }
+        }
+        sc.close();
+    }
+}
+
+//*Time Complexity: O(N) */
+//*Space Complexity: O(N) */
